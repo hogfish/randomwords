845b6401b3f607b124759e35c25048ea05c3cc24
Trish Khoo <trish.khoo@wildfireapp.com>
2012-09-05 17:45:51 +0100

    Added random_email support for any email domain name

--- random_email.rb
+++ random_email.rb
@@ -1,6 +1,7 @@
 dict = File.open('web2')
 lines = dict.readlines
-email = ARGV[0] + "+" + lines[rand(lines.size)].rstrip + "@gmail.com"
+email_input = ARGV[0].split("@")
+email = email_input.first + "+" + lines[rand(lines.size)].rstrip + "@" + email_input.last
  `echo #{email} | pbcopy`
 puts email
 
