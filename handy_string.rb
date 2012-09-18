fed524270f80cbeba3d1b728f81eb4abe7314017
Trish Khoo <trish.khoo@wildfireapp.com>
2012-09-18 18:28:26 +0100

    Removed redundant file

--- handy_string.rb
+++
@@ -1,8 +0,0 @@
-case ARGV[0]
-when 'special_chars'
-  output = '!@£$%^\&*\(\)_+=-±§€#}{][\|:\\;/.,?\>\<\"'
-   `echo #{output} | pbcopy`
-  puts 'Special characters copied to clipboard.'
-else
-  puts 'Invalid parameter. Please use one of the following: special_chars'
-end
