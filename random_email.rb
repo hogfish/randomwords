1d206383c04caacdeeb9d7c4503745aa98ccbd5f
Trish Khoo <trish.khoo@wildfireapp.com>
2012-09-18 18:27:43 +0100

    Change this whole repo up to just be useful code for keyboard shortcuts

--- random_email.rb
+++ random_email.rb
@@ -1,7 +1,5 @@
-dict = File.open('web2')
+dict = File.open('FILEPATH/web2')
 lines = dict.readlines
-email_input = ARGV[0].split("@")
+email_input = 'YOUR_EMAIL'.split("@")
 email = email_input.first + "+" + lines[rand(lines.size)].rstrip + "@" + email_input.last
  `echo #{email} | pbcopy`
-puts email
-
--- random_words.rb
+++ random_words.rb
@@ -1,16 +1,10 @@
-dict = File.open('web2')
+dict = File.open('FILEPATH/web2')
 lines = dict.readlines
 
-if ARGV[0] != nil
-  number_of_words = ARGV[0]
-else
-  number_of_words = 1
-end
+number_of_words = 2
 words = []
 (1..number_of_words.to_i).each do |word|
   words << lines[rand(lines.size)].rstrip.capitalize
 end
 
  `echo #{words.join(" ")} | pbcopy`
-puts words.join(" ")
-
