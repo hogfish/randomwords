0e6ef43c48310d9b0691f999ff462c47c354ec5c
Trish Khoo <trishkhoo@google.com>
2013-10-17 18:53:24 +0100

    Removed unneeded code, added commands for both Windows and OSX

--- random_words.rb
+++ random_words.rb
@@ -1,10 +1,16 @@
-dict = File.open('FILEPATH/web2')
+# Prefix full filepath if you need to call this elsewhere
+dict = File.open('web2')
 lines = dict.readlines
 
 number_of_words = 2
 words = []
-(1..number_of_words.to_i).each do |word|
+(1..number_of_words.to_i).each do
   words << lines[rand(lines.size)].rstrip.capitalize
 end
 
- `echo #{words.join(" ")} | pbcopy`
+puts words.join(" ")
+# Replace the puts statement with the line below to run this on OSX
+# `echo #{words.join(" ")} | pbcopy`
+#
+# Replace the puts statement with the line below to run this on Windows
+# `echo #{words.join(" ")} | clip`
